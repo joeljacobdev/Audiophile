6cdf3606666ac5fd5d413f8fdba52c24512fc7f3
pcforgeek <pcforgeek@outlook.com>
2019-08-06 18:45:05 +0530

    added constants

---
+++ app/src/main/java/com/pcforgeek/audiophile/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.pcforgeek.audiophile.util
+
+
+object Constants {
+    const val ALL_MEDIA_ID = "media:/all"
+    const val FOLDER_MEDIA_ID = "media:/folder"
+    const val ALBUM_MEDIA_ID = "media:/album"
+}
