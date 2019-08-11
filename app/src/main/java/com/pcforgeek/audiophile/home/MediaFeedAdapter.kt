6479d5f1c5eea2cfa102b12aa8823c109f58c756
pcforgeek <pcforgeek@outlook.com>
2019-08-11 09:41:21 +0530

    showing album art in feed

--- app/src/main/java/com/pcforgeek/audiophile/home/MediaFeedAdapter.kt
+++ app/src/main/java/com/pcforgeek/audiophile/home/MediaFeedAdapter.kt
@@ -1,5 +1,6 @@
 package com.pcforgeek.audiophile.home
 
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
@@ -10,9 +11,12 @@ import com.bumptech.glide.Glide
 import com.pcforgeek.audiophile.R
 import com.pcforgeek.audiophile.db.MediaItem
 import kotlinx.android.synthetic.main.media_item_collapsed.view.*
+import android.media.MediaMetadataRetriever
 
 class MediaFeedAdapter(private val songList: MutableList<MediaItem>, private val listener: OnClick) :
     RecyclerView.Adapter<MediaFeedAdapter.MediaItemCollapsedHolder>() {
+
+    var mmr = MediaMetadataRetriever()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaItemCollapsedHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.media_item_collapsed, parent, false)
@@ -43,7 +47,18 @@ class MediaFeedAdapter(private val songList: MutableList<MediaItem>, private val
             if (mediaItem.albumArtUri == null)
                 mediaArt.background = ColorDrawable(Color.DKGRAY)
             else {
-                Glide.with(itemView.context).load(mediaItem.albumArtUri).into(mediaArt)
+                try {
+                    mmr.setDataSource(mediaItem.mediaUri.path)
+                    val data = mmr.embeddedPicture
+                    if (data == null) {
+                        mediaArt.background = ColorDrawable(Color.DKGRAY)
+                    } else {
+                        val bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
+                        Glide.with(itemView.context).load(bitmap).thumbnail(0.1f).into(mediaArt)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
             itemView.setOnClickListener { listener.mediaItemClicked(mediaItem) }
         }
