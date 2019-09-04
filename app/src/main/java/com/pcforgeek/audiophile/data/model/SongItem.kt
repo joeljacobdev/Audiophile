4d2505c42ebdc941011b755a7b44ccb7c64376c0
pcforgeek <pcforgeek@outlook.com>
2019-09-04 19:17:58 +0530

    added SongItem ands CategoryItem model

--- app/src/main/java/com/pcforgeek/audiophile/data/model/SongItem.kt
+++ app/src/main/java/com/pcforgeek/audiophile/data/model/SongItem.kt
@@ -1,4 +1,4 @@
-package com.pcforgeek.audiophile.data
+package com.pcforgeek.audiophile.data.model
 
 import android.net.Uri
 import androidx.room.Entity
@@ -6,19 +6,21 @@ import androidx.room.PrimaryKey
 import com.pcforgeek.audiophile.home.NO_RES
 
 @Entity
-data class MediaItem(
+data class SongItem(
     @PrimaryKey
     val id: String,
-    val artistId: Long,
-    val albumId: Long,
-    val displayTitle: String = "",
-    val album: String = "",
-    val duration: Long,
-    val albumArtUri: Uri?,
+    val artistId: String,
+    val albumId: String,
     val title: String = "",
+    val displayName: String = "",
+    val album: String = "",
     val artist: String = "",
+    val duration: Long,
+    val albumArtPath: String?,
     val genre: String = "",
     val mediaUri: Uri,
+    val favourite: Boolean = false,
+    val playCount: Int = 0,
     val userRating: Long = 0L,
     val playbackRes: Int = NO_RES
 )
