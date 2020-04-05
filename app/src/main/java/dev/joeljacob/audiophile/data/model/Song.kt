57dd7e1770646017e2b0a7ba5901f231e113ea61
Joel Jacob <joeljacobdev@gmail.com>
2020-04-05 16:04:08 +0530

    Fixed: Null value of entries of Mediastore Audio's field crashing app due to not being handled by Song's entity.

--- app/src/main/java/dev/joeljacob/audiophile/data/model/Song.kt
+++ app/src/main/java/dev/joeljacob/audiophile/data/model/Song.kt
@@ -3,8 +3,10 @@ package dev.joeljacob.audiophile.data.model
 import android.net.Uri
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.Index
 import dev.joeljacob.audiophile.home.song.NO_RES
+import dev.joeljacob.audiophile.util.Constants
 
 @Entity(
     tableName = "SongItem",
@@ -18,16 +20,50 @@ data class Song(
     val artistId: String,
     @ColumnInfo(name = "albumId")
     val albumId: String,
-    val title: String = "",
-    val displayName: String = "",
-    val album: String = "",
-    val artist: String = "",
+    val title: String,
+    val displayName: String,
+    val album: String,
+    val artist: String,
     val duration: Long,
     val albumArtPath: String?,
-    val genre: String = "",
+    val genre: String,
     val mediaUri: Uri,
-    val favourite: Boolean = false,
-    val playCount: Int = 0,
-    val userRating: Long = 0L,
-    val playbackRes: Int = NO_RES
-)
+    val favourite: Boolean,
+    val playCount: Int,
+    val userRating: Long,
+    val playbackRes: Int
+) {
+    @Ignore
+    constructor(
+        id: String,
+        artistId: String,
+        albumId: String,
+        title: String? = null,
+        displayName: String? = null,
+        album: String? = null,
+        artist: String? = null,
+        duration: Long,
+        albumArtPath: String? = null,
+        genre: String? = null,
+        mediaUri: Uri,
+        favourite: Boolean = false,
+        playCount: Int = 0,
+        userRating: Long = 0L
+    ) : this(
+        id,
+        artistId,
+        albumId,
+        title ?: Constants.UNKNOWN,
+        displayName ?: Constants.UNKNOWN,
+        album ?: Constants.UNKNOWN,
+        artist ?: Constants.UNKNOWN,
+        duration,
+        albumArtPath,
+        genre ?: Constants.UNKNOWN,
+        mediaUri,
+        favourite,
+        playCount,
+        userRating,
+        NO_RES
+    )
+}
---
+++ app/src/main/java/dev/joeljacob/audiophile/util/Constants.kt
@@ -0,0 +1,5 @@
+package dev.joeljacob.audiophile.util
+
+object Constants {
+    const val UNKNOWN = "unknown"
+}
